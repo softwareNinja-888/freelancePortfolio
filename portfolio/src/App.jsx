3958aafefbc6e5def89abed748955268a7ecbaf0
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-03-01 17:52:13 +0200

    Add analytics for vercel

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,12 +9,16 @@ import { Services } from './components/Services'
 import { Footer } from './components/Footer'
 import { ScrollToTop } from "./components/helper/ScrollToTop"
 
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 function App() {
 
   return (
     <>
       <div className="flex flex-col gap-0 h-full max-w-screen bg-white overflow-x-hidden">
+        <Analytics />
         <Header/>
         <Hero/>
         <About/>
